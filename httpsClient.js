3280e82416033a022bd102d47b250c569ee6fe57
Kai19 <ngyankai@hotmail.com>
2017-07-03 19:06:22 +0000

    made a function that gets and prints Html chunks

---
+++ httpsClient.js
@@ -0,0 +1,36 @@
+const http = require("http");
+
+function getAndPrintHTMLChunks(){
+
+
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  http.get(requestOptions , (res) =>{
+    const statusCode = res.statusCode;
+    const contentType = res.headers['content-type']
+
+
+    let rawData = '';
+    res.setEncoding('utf8');
+    res.on('data', (chunk) =>{
+      rawData += chunk + '\n';
+      console.log(chunk);
+    });
+
+    // res.on('end', (chunk) =>{
+    //   const parsedData = JSON.parse(rawData);
+    //   parsedData.forEach((releaseObject) => {
+    //     console.log(releaseObject.version);
+    //   })
+    // });
+
+
+  })
+
+
+}
+
+getAndPrintHTMLChunks();
